350ec7308a60dbf0266fd7a5317cc8d7417d4634
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-14 10:22:00 -0500

    Initialize development settings

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-var */
+var Webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080/',
+    'webpack/hot/dev-server',
+    './app/index.js'
+  ],
+  output: {
+    path: './dist',
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel?sourceMap'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './',
+    hot: true,
+    proxy: {
+      '/api*': 'http://127.0.0.1:8090'
+    }
+  },
+  devtool: 'source-map',
+  plugins: [
+    new Webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './app/index.html'
+    })
+  ]
+};
