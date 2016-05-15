b5d6842dc6ded3ec13d6c6626ac6534dddd0b483
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-15 07:42:17 -0500

    Add html loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel?sourceMap'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
       }
     ]
   },
