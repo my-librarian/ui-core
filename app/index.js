611fc4adf39d6912e64dc9c5045c7a978f9fb971
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-28 08:10:18 -0500

    Import ui-router and login module

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 import angular from 'angular';
+import uiRouter from 'angular-ui-router';
 
 import app from './views/app';
 import home from './views/home';
+import login from './views/login';
 
 import './styles/less/base.less';
 import './styles/css/material/material-icons.css';
@@ -9,8 +11,10 @@ import './styles/css/material/material-icons.css';
 const appName = 'librarian';
 
 angular.module(appName, [
+  uiRouter,
   app,
-  home
+  home,
+  login
 ]);
 
 angular.element(document)
