b164929803426ba67c10d3751cbbe599b2b7df70
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-28 08:11:33 -0500

    Register master template

--- app/views/app/index.js
+++ app/views/app/index.js
@@ -1,9 +1,11 @@
 import angular from 'angular';
 
+import body from './body';
 import header from './header';
 import headerBanner from './header-banner';
 
 export default angular.module('app', [])
+  .directive(...body)
   .directive(...header)
   .directive(...headerBanner)
   .name;
