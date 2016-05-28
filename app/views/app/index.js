6eb454a055f8e05205d283df9e2056c88f438bd9
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-28 12:12:41 -0500

    Register header user-info

--- app/views/app/index.js
+++ app/views/app/index.js
@@ -4,7 +4,10 @@ import app from './app';
 import body from './body';
 import header from './header';
 import headerBanner from './header-banner';
+import headerUserInfo from './header-user-info';
+import headerUserInfoCtrl from './header-user-info-ctrl';
 import routes from './routes';
+import userInfoAttribute from './user-info-attribute';
 
 export default angular.module('app', [])
   .config(routes)
@@ -12,4 +15,7 @@ export default angular.module('app', [])
   .directive(...body)
   .directive(...header)
   .directive(...headerBanner)
+  .directive(...headerUserInfo)
+  .directive(...userInfoAttribute)
+  .controller('HeaderUserInfoCtrl', headerUserInfoCtrl)
   .name;
