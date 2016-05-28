cb481680ee6296fd659f79686c980680f4071c1b
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-28 08:10:49 -0500

    Register home page route

--- app/views/home/index.js
+++ app/views/home/index.js
@@ -1,7 +1,9 @@
 import angular from 'angular';
 
 import home from './home';
+import routes from './routes';
 
 export default angular.module('home', [])
+  .config(routes)
   .directive(...home)
   .name;
---
+++ app/views/home/routes.js
@@ -0,0 +1,14 @@
+const home = {
+  name: 'home',
+  url: '/home',
+  template: '<home></home>'
+};
+
+export default function routes($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state(home);
+
+  $urlRouterProvider.when('/', '/home');
+  $urlRouterProvider.otherwise('/home');
+}
