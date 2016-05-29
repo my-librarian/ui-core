784b52ac01201e9be5922ecc8446043e3ee8305d
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-29 10:24:18 -0500

    Import authors, common and subjects modules

--- app/index.js
+++ app/index.js
@@ -2,9 +2,12 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 
 import app from './views/app';
+import authors from './views/authors';
 import books from './views/books';
+import common from './common';
 import home from './views/home';
 import login from './views/login';
+import subjects from './views/subjects';
 
 import './styles/less/base.less';
 import './styles/css/material/material-icons.css';
@@ -14,9 +17,12 @@ const appName = 'librarian';
 angular.module(appName, [
   uiRouter,
   app,
+  authors,
   books,
+  common,
   home,
-  login
+  login,
+  subjects
 ]);
 
 angular.element(document)
