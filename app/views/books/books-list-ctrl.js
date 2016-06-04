c5d657bde8bfbcac4642620104a9d767622c4870
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-06-04 13:33:20 -0500

    Get and handle filters

--- app/views/books/books-list-ctrl.js
+++ app/views/books/books-list-ctrl.js
@@ -2,7 +2,27 @@ export default class BooksListCtrl {
 
   constructor(BooksSvc) {
 
-    BooksSvc.getBooks()
+    this.BooksSvc = BooksSvc;
+
+    this.getBooks();
+    this.clearFilters();
+  }
+
+  clearFilters() {
+
+    this.BooksSvc.getFilters()
+      .then(filters => this.filters = filters);
+  }
+
+  getBooks() {
+
+    this.BooksSvc.getBooks()
+      .then(books => this.books = books);
+  }
+
+  onFiltersChange() {
+
+    this.BooksSvc.applyFilters(this.filters)
       .then(books => this.books = books);
   }
 }
