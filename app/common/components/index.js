9bda7f01a45edddfbd9377d3d4627278ee8a6382
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-06-04 13:29:50 -0500

    Export checkbox component

--- app/common/components/index.js
+++ app/common/components/index.js
@@ -1,7 +1,9 @@
 import angular from 'angular';
 
 import attribute from './attribute';
+import checkbox from './checkbox';
 
 export default angular.module('common.components', [])
   .directive(...attribute)
+  .directive(...checkbox)
   .name;
