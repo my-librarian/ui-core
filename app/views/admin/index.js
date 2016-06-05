a2017bac56356770ea0e9fc8000272c4dab71404
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-06-05 14:14:56 -0500

    Export admin module

---
+++ app/views/admin/index.js
@@ -0,0 +1,19 @@
+import angular from 'angular';
+
+import admin from './admin';
+import createAuthor from './create-author';
+import createBook from './create-book';
+import createSubject from './create-subject';
+import helpers from './helpers';
+import routes from './routes';
+
+export default angular
+  .module('admin', [
+    helpers
+  ])
+  .config(routes)
+  .directive(...admin)
+  .directive(...createAuthor)
+  .directive(...createBook)
+  .directive(...createSubject)
+  .name;
