4487aa3dae76475dce62102dbce17d2f79ac29cf
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-06-12 06:01:45 -0500

    Move validity check to blur from format
    Keeping th validity check in format fails to format the model value since it has become just valid.

--- app/views/admin/helpers/rack-no.js
+++ app/views/admin/helpers/rack-no.js
@@ -10,13 +10,9 @@ function link($scope, $elem, $attr, ngModelCtrl) {
 
   function format(value) {
 
-    if(ngModelCtrl.$invalid) {
-      return value;
-    }
-
     value = unFormat(value);
-    value = value.replace(/(\d)([a-zA-Z])/g, '$1-$2');
-    value = value.replace(/([a-zA-Z])(\d)/g, '$1-$2');
+    value = value.replace(/(\d)([a-z])/ig, '$1-$2');
+    value = value.replace(/([a-z])(\d)/ig, '$1-$2');
     value = value.toUpperCase();
     value = `R-${value}`;
 
@@ -30,14 +26,17 @@ function link($scope, $elem, $attr, ngModelCtrl) {
   });
 
   $elem.on('blur', function () {
-    let value = $elem.val();
-    value = format(value);
-    $elem.val(value);
+
+    if (ngModelCtrl.$valid) {
+      let value = $elem.val();
+      value = format(value);
+      $elem.val(value);
+    }
   });
 
   ngModelCtrl.$parsers.push(viewValue => viewValue && format(viewValue));
   ngModelCtrl.$formatters.push(modelValue => modelValue && unFormat(modelValue));
-  ngModelCtrl.$validators.rackNo = (modelView, viewValue) => /^(r|r-)?\d{1,2}-?[a-zA-Z]-?\d{1,2}$/.test(viewValue);
+  ngModelCtrl.$validators.rackNo = (modelView, viewValue) => /^(r|r-)?\d{1,2}-?[a-z]-?\d{1,2}$/i.test(viewValue);
 
 }
 
