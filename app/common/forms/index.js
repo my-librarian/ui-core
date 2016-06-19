45b7178f82e2b6c3b84e44d1a6512efb73f4c368
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-06-19 13:06:06 -0500

    Register borrow and issue forms

--- app/common/forms/index.js
+++ app/common/forms/index.js
@@ -2,6 +2,8 @@ import angular from 'angular';
 
 import authorForm from './author-form';
 import bookForm from './book-form';
+import borrowAuthForm from './borrow-auth-form';
+import issueForm from './issue-form';
 import subjectForm from './subject-form';
 
 import './forms.less';
@@ -10,5 +12,7 @@ export default angular
   .module('admin.forms', [])
   .directive(...authorForm)
   .directive(...bookForm)
+  .directive(...borrowAuthForm)
+  .directive(...issueForm)
   .directive(...subjectForm)
   .name;
