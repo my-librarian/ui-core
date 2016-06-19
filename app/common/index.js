89ba243d1cf742621a3bea682d2df5332d3c4785
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-06-19 13:06:17 -0500

    Register modals

--- app/common/index.js
+++ app/common/index.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 
 import components from './components';
+import dialogs from './dialogs';
 import forms from './forms';
 import filters from './filters';
 import services from './services';
@@ -8,6 +9,7 @@ import services from './services';
 export default angular
   .module('common', [
     components,
+    dialogs,
     forms,
     filters,
     services
