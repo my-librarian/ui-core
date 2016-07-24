b3a6b7380e2fee77294f5a143b35c3badc0911c9
Travis CI <travis@localhost.localdomain>
2016-07-24 08:25:15 -0500

    Deploy only tagged commits

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
-if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  rm -rf dist
+file_deploy () {
+  if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    rm -rf dist
+    npm run build
+    find dist -type f -exec curl -u $USER:$PASS $HOST -T {} \;
+  else
+    echo "This will not deploy!"
+  fi
+}
+
+os_deploy() {
   npm run build
-  find dist -type f -exec curl -u $USER:$PASS $HOST -T {} \;
-else
-  echo "This will not deploy!"
-fi
+  cd dist
+  git init
+  echo "$TRAVIS_TAG" > version.txt
+  git config --global user.email "vipranarayan14@gmail.com"
+  git config --global user.name "Travis CI"
+  git add --all
+  git commit -am "Deploy version $TRAVIS_TAG"
+}
