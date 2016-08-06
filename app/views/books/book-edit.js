b2c11624a07b970c1a4511b75f3773661635c064
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-08-06 14:11:24 -0500

    Authenticate edit page

--- app/views/books/book-edit.js
+++ app/views/books/book-edit.js
@@ -3,14 +3,24 @@ import template from './book-edit.html';
 
 import './book-edit.less';
 
-const bookEdit = {
-  controller,
-  controllerAs: 'vm',
-  restrict: 'E',
-  template
-};
+/*@ngInject*/
+function bookEdit($state, LoginSvc) {
+
+  return {
+    link() {
+
+      if (!LoginSvc.userLevel.isModerator) {
+        $state.go('login');
+      }
+    },
+    controller,
+    controllerAs: 'vm',
+    restrict: 'E',
+    template
+  };
+}
 
 export default [
   'bookEdit',
-  () => bookEdit
+  bookEdit
 ];
