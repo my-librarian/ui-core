ac1f7a4e21afabc9459595dbf5f6e9c33023f4ad
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-08-13 12:05:30 -0500

    Keyboard shortcuts

--- app/views/app/app.js
+++ app/views/app/app.js
@@ -1,11 +1,40 @@
 import template from './app.html';
 
-const app = {
-  restrict: 'E',
-  template
-};
+/*@ngInject*/
+function app($document, $state) {
+
+  function getKeyMap() {
+
+    return {
+      a: () => $state.go('authors.list'),
+      b: () => $state.go('books.list'),
+      h: () => $state.go('home'),
+      l: () => $state.go('login'),
+      s: () => $state.go('subjects.list')
+    };
+  }
+
+  return {
+    link() {
+
+      $document.on('keyup', event => {
+
+        if (event.altKey && event.ctrlKey) {
+
+          const goTo = getKeyMap()[event.key];
+
+          if (goTo) {
+            goTo();
+          }
+        }
+      });
+    },
+    restrict: 'E',
+    template
+  };
+}
 
 export default [
   'app',
-  () => app
+  app
 ];
