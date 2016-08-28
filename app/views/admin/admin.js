92ae039d96c0503485ac9589cf809b66ea9ef598
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-08-28 09:50:24 -0500

    Redirect to home on logging out from an admin page

--- app/views/admin/admin.js
+++ app/views/admin/admin.js
@@ -6,12 +6,24 @@ import './admin.less';
 function admin($state, LoginSvc, TitleSvc) {
 
   return {
-    link() {
+    link($scope) {
       TitleSvc.setTitle('Admin');
 
-      if(!LoginSvc.userLevel.isModerator) {
+      if (!LoginSvc.userLevel.isModerator) {
         $state.go('login');
       }
+
+      const destroyWatch = $scope.$watch(
+        () => LoginSvc.user.details.userid,
+        newValue => {
+          if (newValue === undefined) {
+            $state.go('home');
+          }
+        }
+      );
+
+      $scope.$on('$destroy', destroyWatch);
+
     },
     restrict: 'E',
     template
