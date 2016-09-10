e875d31b3e15ff059b13ae6e9eeb683941aab048
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-09-10 12:07:02 -0500

    Fix spelling mistakes

--- app/i18n/sa.js
+++ app/i18n/sa.js
@@ -5,13 +5,13 @@ export default {
   ADMINISTRATION: 'प्रशासनम्',
   AUTHORS: 'ग्रन्थकर्ताः',
   BOOKS: 'पुस्तकानि',
-  DEPARTMENT_OF_SANSKRIT: 'संस्कृत विभाग - RKMVC',
-  HOME: 'मुखपृष्ठ',
+  DEPARTMENT_OF_SANSKRIT: 'संस्कृत विभागः - RKMVC',
+  HOME: 'मुखपृष्ठम्',
   LIBRARY_ADMINISTRATION: 'ग्रन्थालय प्रशासनम्',
   LOG_IN: 'सम्प्रवेश',
   LOG_OUT: 'बहिर्गच्छ',
-  MY_LIBRARIAN: 'मम ग्रन्थालयक',
-  PROFILE: 'व्यक्तिविवरण',
+  MY_LIBRARIAN: 'मम ग्रन्थालयकः',
+  PROFILE: 'व्यक्तिविवरणम्',
   SUBJECTS: 'विषयाः',
   WELCOME_MESSAGE: 'स्वागतम्, {{name}}!'
 };
